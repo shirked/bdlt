12e40a538b91b22f66b280df9559a62e2621606e
shirked <deep.17080@sakec.ac.in>
2026-05-17 22:49:43 +0530

    Nothing

--- hyperDown.sh
+++ hyperDown.sh
@@ -1,9 +1,19 @@
-./network.sh down
-docker rm -f $(docker ps -a -q)
-docker volume rm $(docker volume ls -q)
-docker network prune -f
+# 1. Navigate to your fabric-samples test network folder
 cd ~/go/src/github.com/fabric-samples/test-network
+
+# 2. Use the official fabric script to cleanly bring the network down
+./network.sh down
+
+# 3. Clean up lingering Docker artifacts cleanly
+docker ps -a -q | xargs -r docker rm -f
+docker volume ls -q | xargs -r docker volume rm
+docker network prune -f
+
+# 4. Wipe out all locally generated crypto material, channel configurations, and logs
 sudo rm -rf organizations/ channel-artifacts/ system-genesis-block/ log.txt *.tar.gz
-git checkout -- organizations/
-cd ~/go/src/github.com/fabric-samples/explorer
-docker-compose down -v
+
+# 5. Clear out cached Docker images to save space
+docker image prune -a -f
+
+# 6. Go home safely (without running invalid compose commands)
+cd ~
