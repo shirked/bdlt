d4b9327c1802fecac622ddcf505dff7ed92a5149
shirked <deep.17080@sakec.ac.in>
2026-05-17 20:29:40 +0530

    Hyperledger down

---
+++ hyperDown.sh
@@ -0,0 +1,9 @@
+./network.sh down
+docker rm -f $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
+docker network prune -f
+cd ~/go/src/github.com/fabric-samples/test-network
+sudo rm -rf organizations/ channel-artifacts/ system-genesis-block/ log.txt *.tar.gz
+git checkout -- organizations/
+cd ~/go/src/github.com/fabric-samples/explorer
+docker-compose down -v
